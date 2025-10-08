be8a14eecb33ab6fbc362c646f559969fe1bcb93
Vyncen <vyncen.lara@gmail.com>
2025-10-07 22:32:56 -0600

    H05: Finish the lasted homework

--- Hw06/Zapatos/admin.py
+++ Hw06/Zapatos/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer, Shoe, Order, OrderItem
 
-# Register your models here.
+admin.site.register(Customer)
+admin.site.register(Shoe)
+admin.site.register(Order)
+admin.site.register(OrderItem)
